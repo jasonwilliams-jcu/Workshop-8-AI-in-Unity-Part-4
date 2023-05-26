530ce09be07e573540b1d75cf8b55eed783eb861
jasonwilliams-jcu <jason.williams1@my.jcu.edu.au>
2023-05-26 15:57:50 +1000

    complete crowd flee

--- Workshop 8 Moving as One/Assets/AIControl.cs
+++ Workshop 8 Moving as One/Assets/AIControl.cs
@@ -8,6 +8,10 @@ public class AIControl : MonoBehaviour {
     GameObject[] goalLocations;
     NavMeshAgent agent;
     Animator anim;
+    float speedMult;
+    float detectionRadius = 20;
+    float fleeRadius = 10;
+
 
     void Start() {
 
@@ -16,17 +20,45 @@ public class AIControl : MonoBehaviour {
         int i = Random.Range(0, goalLocations.Length);
         agent.SetDestination(goalLocations[i].transform.position);
         anim = this.GetComponent<Animator>();
-        anim.SetTrigger("isWalking");
         anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
-        float sm = Random.Range(0.5f, 2);
-        anim.SetFloat("speedMult", sm);
-        agent.speed *= sm;
+        ResetAgent();
+
     }
 
+    void ResetAgent()
+    {
+        speedMult = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMult", speedMult);
+        agent.speed *= speedMult;
+        anim.SetTrigger("isWalking");
+        agent.angularSpeed = 120;
+        agent.ResetPath();
+    }
+
+    public void DetectNewObstacle(Vector3 position)
+    {
+        if(Vector3.Distance(position, this.transform.position) < detectionRadius)
+        {
+            Vector3 fleeDirection = (this.transform.position - position). normalized;
+            Vector3 newGoal = this.transform.position + fleeDirection * fleeRadius;
+
+            NavMeshPath path = new NavMeshPath();
+            agent.CalculatePath(newGoal, path);
+
+            if(path.status != NavMeshPathStatus.PathInvalid)
+            {
+                agent.SetDestination(path.corners[path.corners.Length - 1]);
+                anim.SetTrigger("isRunning");
+                agent.speed = 10;
+                agent.angularSpeed = 500;
+            }
+        }
+    }
 
     void Update() {
         if(agent.remainingDistance < 1)
         {
+            ResetAgent();
             int i = Random.Range(0, goalLocations.Length);
             agent.SetDestination(goalLocations[i].transform.position);
         }
